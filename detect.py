aa11a08aa2c8930dcf14c94b715c1b299f2e5d44
Valentin <valentinvial42.vv@gmail.com>
2023-09-30 09:47:57 +0200

    sqdds

--- detect.py
+++ detect.py
@@ -41,15 +41,19 @@ def detect_v2(weights) :
     df["postprocess time"] = speed["postprocess"]
     df.to_csv("./results.csv", index=False)
 
+    print("Results!")
+
+
     df_class = pd.DataFrame(ap_class_index, columns=["class_index"])
     df_class["class_name"] = [metrics.names[i] for i in ap_class_index]
     maps = []
-    
     for i in ap_class_index :
         maps.append(metrics.class_result(i))
 
+    df_class["mAP"] = maps
+    df_class.to_csv("./results_class.csv", index=False)
 
-
+    print("Results_per_class!")
 
 
 
